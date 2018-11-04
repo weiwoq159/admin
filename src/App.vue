55c32ce7f85d5eb8d6421af6ec9aa728eeac5b94
maqqiang <weiwoq159@qq.com>
2018-11-04 20:53:07 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+        <el-container>
+            <el-header>
+
+            </el-header>
+            <el-main>
+                <!-- Main content -->
+                
+            </el-main>
+            <el-footer>
+                <!-- Footer content -->
+            </el-footer>
+        </el-container>
+        <router-view />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        data() {
+            return {
+                username: '张丽丽'
+            }
+        }
+    }
+
+</script>
+
+<style lang='stylus'>
+
+</style>
